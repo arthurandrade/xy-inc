005eae575983b8bc80eb84ed5e77c3f9c035cdf0
Arthur Andrade <arthurandrade186@gmail.com>
2016-02-05 07:17:53 -0200

    rest funcionando
    fazer validações e controle de erro

--- src/com/ServicoCorreios/ConsultaCorreiosJSON.java
+++ src/com/ServicoCorreios/ConsultaCorreiosJSON.java
@@ -1,5 +1,7 @@
 package com.servicoCorreios;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,28 +14,42 @@ import javax.ws.rs.core.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.beans.CorreioBean;
+import com.consultaCorreios.ConsultaCorreios;
+import com.consultaCorreios.ConsultaCorreiosImpl;
+import com.utils.CriaSaida;
+
 
 @Path("/ConsultaCorreiosJSON")
 public class ConsultaCorreiosJSON {
 
+	private ConsultaCorreios consultaCorreios;
+	
 	  @Path("{f}")
 	  @GET
 	  @Produces("application/json")
-	  public Response convertFtoCfromInput(@PathParam("f") String f) throws JSONException {
+	  public Response convertFtoCfromInput(@PathParam("f") String f) throws JSONException, UnsupportedEncodingException {
 
-		JSONObject jsonObject = new JSONObject();
-		
-		Pattern pattern = Pattern.compile("\\d{8,8}");
-        Matcher matcher = pattern.matcher(f);
-        if( matcher.find() ){
-        	jsonObject.put("cep", f);
-        }else{
-        	jsonObject.put("locadouro", f);
-        }
-		 
-		
-		
-		return Response.status(200).entity(jsonObject.toString()).build();
+		  Pattern pattern = Pattern.compile("\\d{8,8}");
+			Matcher matcher = pattern.matcher(f);
+			String texto = "";			
+			consultaCorreios = new ConsultaCorreiosImpl();
+			List<CorreioBean> lisrEnd = null;
+			try {
+				if (matcher.find()) {
+					
+					lisrEnd = consultaCorreios.buscarCep(f);	
+					
+				} else {
+					lisrEnd = consultaCorreios.buscarEndereco(f);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			texto = CriaSaida.montaMensagemRetornoJSON(lisrEnd);
+			System.out.println(texto);
+		return Response.status(200).entity(texto).build();
 	  }
 
 }
